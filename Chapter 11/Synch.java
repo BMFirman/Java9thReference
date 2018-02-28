3ba45040f2fece2e5d3a79a4ebfb3dc9c154922a
BMFirman <bfirman@fastmail.com>
2018-03-01 12:53:37 +1300

    Chapter 11 complete

---
+++ Chapter 11/Synch.java
@@ -0,0 +1,55 @@
+// this program is now synchronized
+
+/* general form
+    synchronized(objRef) {
+        // statements to synchronize
+    }
+*/
+
+class Callme {
+    // allow only one object to access the call method at a time
+    // note this is the method on the same instance
+    synchronized void call(String msg) {
+        System.out.print("[" + msg);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+        System.out.println("]");
+    }
+}
+
+class Caller implements Runnable {
+    String msg;
+    Callme target;
+    Thread t;
+
+    public Caller(Callme targ, String s) {
+        target = targ;
+        msg = s;
+        t = new Thread(this);
+        t.start();
+    }
+    public void run() {
+        target.call(msg);
+    }
+}
+
+class Synch {
+    public static void main(String[] args) {
+        Callme target = new Callme();
+        Caller ob1 = new Caller(target, "Hello");
+        Caller ob2 = new Caller(target, "Synchronized");
+        Caller ob3 = new Caller(target, "World");
+
+        // wait for threads to finish
+        try {
+            ob1.t.join();
+            ob2.t.join();
+            ob3.t.join();
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+    }
+}
