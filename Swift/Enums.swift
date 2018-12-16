8d89d8cfede55c68bbf98c41a9836279fb3b1b8b
BMFirman <bfirman@fastmail.com>
2018-12-16 20:56:00 +1300

    Enums

--- Swift/Enums.swift
+++ Swift/Enums.swift
@@ -25,3 +25,43 @@ enum Drink: CaseIterable {
 
 let numChoices = Drink.allCases.count
 print("\(numChoices) drink choices")
+
+enum Barcode {
+     case upc(Int, Int, Int, Int)
+     case qrCode(String)
+}
+
+var barcode = Barcode.upc(1,2,3,4)
+barcode = .qrCode("Hello")
+
+// Raw Values
+
+enum ASCIIControlCharacter: Character {
+    case tab = "\t"
+}
+
+// Implicit assignment
+enum Planet: Int {
+    // It follows that venus = 2, earth = 3
+    case mercury = 1, venus, earth
+}
+
+enum Compass2: String {
+    // The implicit raw values are the following as strings
+    case north, south, east, west
+}
+
+let earthsOrder = Planet.earth.rawValue
+let possiblePlanet = Planet(rawValue: 3)
+
+let positionToFind = 11
+if let somePlanet = Planet(rawValue: positionToFind) {
+    switch somePlanet {
+        case .earth:
+            print("Harmless")
+        default:
+            print("Not safe for humans")
+    }
+} else {
+    print("There isn't a planet at that postion")
+}
